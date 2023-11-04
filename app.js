48341b0f28e0fcb4ac7b9330b8df759863ccc233
Kayla Dale <kmeyers119@gmail.com>
2023-11-03 22:05:59 -0500

    created mad lib sentence, btn & 4 functions

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+
+function adj() {
+    let adj = window.prompt("ENTER AN ADJECTIVE");
+    document.getElementById("adjective").innerHTML = adj;
+}
+function emotion() {
+    let emotion = window.prompt("ENTER AN EMOTION");
+    document.getElementById("emotion").innerHTML = emotion;
+}
+function place() {
+    let place = window.prompt("ENTER A PLACE");
+    document.getElementById("place").innerHTML = place;
+}
+function thing() {
+     let thing = window.prompt("ENTER A THING");
+     document.getElementById("thing").innerHTML = thing;
+}
+
